a8b2c83b3c9939eabbf545db84440652bafc9786
Brandon Espinoza <espinozab100@gmail.com>
2016-05-31 23:26:42 -0500

    Created Barbers and Walks Controllers and Walk Model

---
+++ app/controllers/walks_controller.rb
@@ -0,0 +1,7 @@
+class WalksController < ApplicationController
+
+  def index
+
+  end
+
+end
---
+++ db/migrate/20160601042455_create_walks.rb
@@ -0,0 +1,10 @@
+class CreateWalks < ActiveRecord::Migration
+  def change
+    create_table :walks do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
+  end
+end
